63aeb0d55cb0c858fa85c3852abed950781c499e
shreegowri <gaganandh@gmail.com>
2022-06-12 09:39:17 +0530

    [shreegowri]welcome message

---
+++ EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EMPLOYEEWAGEPROBLEM
+{
+    class program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("welcome to employee wage problem");
+        }
+    }
+}
