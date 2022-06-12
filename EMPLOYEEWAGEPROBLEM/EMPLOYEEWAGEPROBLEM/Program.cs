6bb09d0e106baab61ee1a1ec8aaaf50384a1e860
shreegowri <gaganandh@gmail.com>
2022-06-12 09:55:40 +0530

    [shreegowri]Solve using switchcase

--- EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/Program.cs
+++ EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/Program.cs
@@ -5,8 +5,8 @@
         public static void Main(string[]args)
         {
             Console.WriteLine("welcome to employee wage problem");
-            PartTimeEmpWage prg = new PartTimeEmpWage();
-            prg.CheckPartTimeEmpWage();
+            SwitchCaseProb prg = new SwitchCaseProb();
+            prg.CalculateEmpWage();
         }
     }
 }
