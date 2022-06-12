6d4c4a1ef97921ad60cd1f10ebb7b5b859ae160c
shreegowri <gaganandh@gmail.com>
2022-06-12 09:46:07 +0530

    [shreegowri]Employee present absent

--- EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/Program.cs
+++ EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[]args)
         {
             Console.WriteLine("welcome to employee wage problem");
+            //UC1
+            EmpPresentAbsent prg = new EmpPresentAbsent();
+            prg.CheckEmpPresentAbsent();
         }
     }
 }
