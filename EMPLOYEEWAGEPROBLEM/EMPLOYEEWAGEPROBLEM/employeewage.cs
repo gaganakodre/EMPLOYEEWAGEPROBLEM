7b1692bb741f4325b6914429e5303ee1feda5798
shreegowri <gaganandh@gmail.com>
2022-06-15 21:19:24 +0530

    [shreegowri]Adding monthly wage and create array to store company name and monthly wage

--- EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/employeewage.cs
+++ EMPLOYEEWAGEPROBLEM/EMPLOYEEWAGEPROBLEM/employeewage.cs
@@ -14,11 +14,20 @@ namespace EMPLOYEEWAGEPROBLEM
         float EmpDailyWage = 0;
         public float TotalWage = 0;
         private Dictionary<String, Company> Companies = new Dictionary<String, Company>();
+        public string[] CompanyList;
+        public int ArrayIndex = 0;
+
+        public EmployeeWageComputation(int Number)
+        {
+            CompanyList = new string[2 * Number];
+        }
 
         public void AddCompany(String CompanyName, int EmpWagePerhour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHours_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
         {
             Company company = new Company(CompanyName.ToLower(), EmpWagePerhour, FullTime_WorkingHrs_PerDay, PartTime_WorkingHours_PerDay, MAX_WORKING_HRS, MAX_WORKING_DAYS);
             Companies.Add(CompanyName.ToLower(), company);
+            CompanyList[ArrayIndex] = CompanyName;
+            ArrayIndex++;
         }
 
         public int IsEmployeePresent()
@@ -28,6 +37,7 @@ namespace EMPLOYEEWAGEPROBLEM
 
         public void CalculateEmpWage(string CompanyName)
         {
+            float Monthly_Wage = 0;
             int DayNumber = 1;
             int EmpWorkingHours = 0;
             int TotalWorkingHrs = 0;
@@ -60,9 +70,20 @@ namespace EMPLOYEEWAGEPROBLEM
                 TotalWage += EmpDailyWage;
                 DayNumber++;
                 TotalWorkingHrs += EmpWorkingHours;
-            }
-            Console.WriteLine("Total working days :" + (DayNumber - 1) + "\n Total working hours :" + TotalWorkingHrs + "");
+                Monthly_Wage += TotalWage;
 
+            } 
+            CompanyList[ArrayIndex] = Convert.ToString(Monthly_Wage);
+                ArrayIndex++;
+            Console.WriteLine("Total working days :" + (DayNumber - 1) + "\n Total working :" + TotalWorkingHrs + "" + "\n monthlywage :" + Monthly_Wage + "");
+
+        }
+        public void displayArray()
+        {
+            for(int i=0; i<CompanyList.Length; i+=2)
+            {
+                Console.WriteLine("Monthly wage for {0} is {1}" ,CompanyList[i], CompanyList[i+1]);  
+            }
         }
 
     }
